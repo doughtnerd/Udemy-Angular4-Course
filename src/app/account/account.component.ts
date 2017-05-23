75d26aa63ef49fe3c191b9a44275fada98074be6
TROGDOR\dough <doughtnerd@gmail.com>
2017-05-22 20:36:01 -0600

    Completed module

---
+++ src/app/account/account.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {LoggingService} from "../logging.service";
+import {AccountService} from "../accounts.service";
+
+@Component({
+  selector: 'app-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.css'],
+  // providers: [LoggingService]
+})
+export class AccountComponent {
+  @Input() account: {name: string, status: string};
+  @Input() id: number;
+
+  constructor(private loggingService: LoggingService, private  aService: AccountService) {
+
+  }
+
+  onSetTo(status: string) {
+    this.aService.updateStatus(this.id, status);
+    // this.loggingService.logStatusChange(status);
+  }
+}
---
+++ src/app/new-account/new-account.component.ts
@@ -0,0 +1,21 @@
+import { Component} from '@angular/core';
+import {LoggingService} from "../logging.service";
+import {AccountService} from "../accounts.service";
+
+@Component({
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css'],
+  // providers: [LoggingService]
+})
+export class NewAccountComponent {
+
+  constructor(private loggingService: LoggingService, private aService: AccountService) {
+
+  }
+
+  onCreateAccount(accountName: string, accountStatus: string) {
+    this.aService.addAccount(accountName, accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
+  }
+}
