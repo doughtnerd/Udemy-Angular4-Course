fa6c312f91c44bc3f1633b9698a139d544729498
TROGDOR\dough <doughtnerd@gmail.com>
2017-06-02 07:51:40 -0600

    Finished observables section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UsersService} from "./users.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  user1Activated = false;
+  user2Activated = false;
+
+  constructor(private service: UsersService) {
+
+  }
+
+  ngOnInit() {
+    this.service.userActivated.subscribe((id: number) => {
+      if (id === 1) {
+        this.user1Activated = true;
+        this.user2Activated = false;
+      } else if (id === 2) {
+        this.user2Activated = true;
+        this.user1Activated = false;
+      }
+
+    });
+  }
 }
