97a4e278e12abcb40c0beed57e15f2beeb10d953
TROGDOR\dough <doughtnerd@gmail.com>
2017-06-04 01:01:37 -0600

    Finished template drive form, on to reactive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {NgForm} from "@angular/forms";
 
 @Component({
@@ -7,11 +7,52 @@ import {NgForm} from "@angular/forms";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild('f') form: NgForm;
+
+  defaultQuestion = 'pet';
+  answer = '';
+  genders = ['male', 'female'];
+  submitted = false;
+
+  user = {
+    username: '',
+    email: '',
+    question: '',
+    answer: '',
+    gender: ''
+  };
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.form.setValue(
+    //   {
+    //     userData: {
+    //       username: suggestedName,
+    //       email: ''
+    //     },
+    //     secret: 'pet',
+    //     questionAnswer: '',
+    //     gender: 'male'
+    //   }
+    // );
+    this.form.form.patchValue({userData: {username: suggestedName}});
   }
 
-  onSubmit(form: NgForm) {
-    console.log(form);
+  //
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  onSubmit() {
+    this.submitted = true;
+    this.user.username = this.form.value.userData.username;
+    this.user.email = this.form.value.userData.email;
+    this.user.question = this.form.value.secret;
+    this.user.answer = this.form.value.questionAnswer;
+    this.user.gender = this.form.value.gender;
+    console.log(this.form);
+
+    this.form.reset();
   }
 }
