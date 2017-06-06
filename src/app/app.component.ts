c5347c6d7ebe5a176462b7af9efba75f1fcaa46a
TROGDOR\dough <doughtnerd@gmail.com>
2017-06-05 23:40:13 -0600

    Finished reactive forms section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {FormArray, FormControl, FormGroup, NgForm, Validators} from "@angular/forms";
+import {Form, FormArray, FormControl, FormGroup, NgForm, Validators} from "@angular/forms";
+import {Observable} from "rxjs/Observable";
 
 @Component({
   selector: 'app-root',
@@ -11,23 +12,62 @@ export class AppComponent implements OnInit{
 
   signupForm: FormGroup;
 
+  forbiddenUsernames = ['Chris', 'Anna'];
+
   ngOnInit() {
     this.signupForm = new FormGroup({
       userData: new FormGroup({
-        username: new FormControl(null, Validators.required),
-        email: new FormControl(null, [Validators.required, Validators.email]),
+        username: new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
+        email: new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails),
       }),
       gender: new FormControl('male'),
       hobbies: new FormArray([])
+    });
+    this.signupForm.statusChanges.subscribe((value) => {
+      console.log(value);
+    });
+    this.signupForm.setValue({
+      userData: {
+        username: 'Max',
+        email: 'max@test.com'
+      },
+      gender: 'male',
+      hobbies: []
+    });
+    this.signupForm.patchValue({
+      userData: {
+        username: 'Anna'
+      }
     });
   }
 
   onSubmit() {
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
 
   onAddhobby() {
     const control = new FormControl(null, Validators.required);
     (<FormArray> this.signupForm.get('hobbies')).push(control);
+  }
+
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
+    return null;
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const prom = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({'emailIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return prom;
   }
 }
